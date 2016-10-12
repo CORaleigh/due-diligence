82bc160e10d0379c7406cccf30d40bd7c007c929
justingreco <justin.greco@raleighnc.gov>
2016-10-12 11:33:30 -0400

    added form inputs

--- app/src/app.js
+++ app/src/app.js
@@ -6,12 +6,7 @@ import 'angular-aria';
 import 'angular-material';
 import 'angular-ui-router';
 import AppController from 'src/AppController';
-import AdminController from 'src/AdminController';
-import MainController from 'src/MainController';
-import LoginController from 'src/LoginController';
-import SignupController from 'src/SignupController';
-import ForgotController from 'src/ForgotController';
-import ResetController from 'src/ResetController';
+
 
 export default angular.module( 'starter-app', [ 'ngMaterial', 'ngAnimate', 'ui.router'] )
   .config(($mdIconProvider, $mdThemingProvider, $httpProvider, $stateProvider, $urlRouterProvider) => {
@@ -32,73 +27,17 @@ export default angular.module( 'starter-app', [ 'ngMaterial', 'ngAnimate', 'ui.r
       .icon("update", "./assets/svg/update.svg", 24)          
       .icon("delete", "./assets/svg/delete.svg", 24);       
     $mdThemingProvider.theme('default')
-      .primaryPalette('green')
-      .accentPalette('blue');
-    $httpProvider.defaults.headers.delete = { 'Accept' : 'application/json' }; 
-    $urlRouterProvider.otherwise('/login');
+      .primaryPalette('blue')
+      .accentPalette('red');
+    $httpProvider.defaults.headers.delete = { 'Accept' : 'application/json' };
+    $urlRouterProvider.otherwise('/form');
     $stateProvider
-    .state('login', {
-      url: '/login', 
-      templateUrl: './templates/login.html',
-      params: {message: null, email: null}
-    })
-    .state('signup', {
-      url: '/signup', 
-      templateUrl: './templates/signup.html'
-    })    
-    .state('form', {
-        url: '/form',
-        templateUrl: './templates/main.html',
-        params: {user: null, token: null}
-    })
-    .state('form.id', {
-        url: '/:id',
-        templateUrl: './templates/main.html'     
-    })    
-    .state('admin', {
-        url: '/admin',
-        templateUrl: './templates/admin.html',
-        params: {tab: 0, user: null, token: null}   
-    })
-    .state('admin.tab', {
-        url: '/:tab',
-        templateUrl: './templates/admin.html',
-        params: {tab: 0, user: null, token: null}     
-    })
-    .state('forgot', {
-      url: '/forgot',
-      templateUrl: './templates/forgot.html'
-    })
-    .state('reset', {
-      url: '/reset/:token',
-      templateUrl: './templates/reset.html'
-    })    
-    .state('reset.token', {
-      url: '/reset/:token',
-      templateUrl: './templates/reset.html',
-      params: {token: null}
-    });
+      .state('form', {
+          url: '/form',
+          templateUrl: './templates/main.html',
+          params: {user: null, token: null}
+      });
+
   })
-  .controller('AppController', AppController)
-  .controller('AdminController', AdminController)
-  .controller('MainController', MainController)
-  .controller('LoginController', LoginController)
-  .controller('SignupController', SignupController)  
-  .controller('ForgotController', ForgotController)   
-  .controller('ResetController', ResetController)         
-  .factory('location', [
-    '$location',
-    '$route',
-    '$rootScope',
-    function ($location, $route, $rootScope) {
-        $location.skipReload = function () {
-            var lastRoute = $route.current;
-            var un = $rootScope.$on('$locationChangeSuccess', function () {
-                $route.current = lastRoute;
-                un();
-            });
-            return $location;
-        };
-        return $location;
-    }
-]);  
+  .controller('AppController', AppController);
+
