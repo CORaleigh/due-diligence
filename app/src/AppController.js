c1c8c9665abf9a19341bc59e4e4723f50a0cea60
justingreco <justin.greco@raleighnc.gov>
2016-10-12 22:35:28 -0400

    added placeholder splash dialog

--- app/src/AppController.js
+++ app/src/AppController.js
@@ -4,11 +4,26 @@
  * @param $mdSidenav
  * @constructor
  */
-function AppController($mdSidenav, $http, $rootScope, $scope, $location, $mdMedia, $window,  $httpParamSerializerJQLike) {
+function AppController($http, $scope, $httpParamSerializerJQLike, $mdDialog) {
     'use strict';
     var self = this;
     var map = null,
         view = null;
+      self.showAlert = function(ev) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        // Modal dialogs should fully cover application
+        // to prevent interaction outside of dialog
+        $mdDialog.show(
+          $mdDialog.alert()
+            .parent(angular.element(document.querySelector('#popupContainer')))
+            .clickOutsideToClose(true)
+            .title('This is an alert title')
+            .textContent('You can specify some description text in here.')
+            .ariaLabel('Alert Dialog Demo')
+            .ok('Got it!')
+            .targetEvent(ev)
+        );
+      };        
     self.data = {contact: 'Justin Greco', 
         phone: '919-996-2523', 
         mobile: '919-996-2523',
@@ -140,4 +155,4 @@ function AppController($mdSidenav, $http, $rootScope, $scope, $location, $mdMedi
 
 
 }
-export default ['$mdSidenav', '$http', '$rootScope', '$scope', '$location', '$mdMedia', '$window', '$httpParamSerializerJQLike', AppController];
+export default ['$http', '$scope', '$httpParamSerializerJQLike', '$mdDialog', AppController];
