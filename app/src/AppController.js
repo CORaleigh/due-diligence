7213e115299594b562abb744a26685cfa7a2655c
justingreco <justin.greco@raleighnc.gov>
2016-10-13 14:08:05 -0400

    get property/zoning for Durham Co properties

--- app/src/AppController.js
+++ app/src/AppController.js
@@ -52,7 +52,7 @@ function AppController($http, $scope, $httpParamSerializerJQLike, $mdDialog) {
     self.getZoning = function (QueryTask, query) {
         query.outFields = ['ZONING'];
         query.returnGeometry = false;
-        var queryTask = new QueryTask("http://maps.raleighnc.gov/arcgis/rest/services/Planning/Zoning/MapServer/0")
+        var queryTask = new QueryTask("http://maps.raleighnc.gov/arcgis/rest/services/Planning/Zoning/MapServer/0");
         queryTask.execute(query, function (result) {
             if (result.features.length > 0) {
                 self.data.zoning = result.features[0].attributes.ZONING;
@@ -60,10 +60,10 @@ function AppController($http, $scope, $httpParamSerializerJQLike, $mdDialog) {
             }
         });
     };
-    self.getProperty = function (point) {
+    self.getProperty = function (point, id) {
         require(["esri/tasks/QueryTask", "esri/tasks/query"], function (QueryTask, Query) {
             var queryTask = new QueryTask(
-                'https://maps.raleighnc.gov/arcgis/rest/services/Parcels/MapServer/0'
+                'https://maps.raleighnc.gov/arcgis/rest/services/Parcels/MapServer/' + id
             );
             var query = new Query();
             query.returnGeometry = true;
@@ -76,6 +76,10 @@ function AppController($http, $scope, $httpParamSerializerJQLike, $mdDialog) {
                     self.data.pin1 = result.features[0].attributes.PIN_NUM;
                     $scope.$digest();
                     self.getZoning(QueryTask, query);
+                } else {
+                    if (id === '0') {
+                        self.getProperty(point, '1');
+                    }
                 }
             });
         });
@@ -103,7 +107,7 @@ function AppController($http, $scope, $httpParamSerializerJQLike, $mdDialog) {
                 self.getProperty(new Point({
                     longitude: address.geometry.x,
                     latitude: address.geometry.y
-                }));
+                }), '0');
             });
         }
     };
@@ -124,10 +128,10 @@ function AppController($http, $scope, $httpParamSerializerJQLike, $mdDialog) {
         ], function (Map, VectorTileLayer, FeatureLayer, SimpleRenderer) {
             console.log('load');
             if (!map) {
-                  map = new Map("viewDiv", {
+                map = new Map("viewDiv", {
                     center: [-78.65, 35.8],
                     zoom: 10
-                  });
+                });
 
                 var tileLyr = new VectorTileLayer("https://www.arcgis.com/sharing/rest/content/items/bf79e422e9454565ae0cbe9553cf6471/resources/styles/root.json");
                 map.addLayer(tileLyr);
